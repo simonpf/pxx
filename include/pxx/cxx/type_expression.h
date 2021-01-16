44bb602faa519992a3023a003f61a230b49421a2
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-01-16 20:24:14 +0100

    Added replacement functionality to type expression iterator.

--- include/pxx/cxx/type_expression.h
+++ include/pxx/cxx/type_expression.h
@@ -3,9 +3,9 @@
  * Defines the TypeExpression class for manipulating expression involving
  * types.
  */
-#include <string>
-#include <locale>
 #include <iostream>
+#include <locale>
+#include <string>
 
 #include <pxx/cxx/common.h>
 
@@ -14,136 +14,134 @@ namespace cxx {
 namespace types {
 
 namespace detail {
-    inline bool is_identifier_char(char c) { return c == '_' || std::isalnum(c) ; }
-    inline bool is_whitespace(char c) { return std::isblank(c); }
-}
+/// Whether a given char can make up an identifier.
+inline bool is_identifier_char(char c) { return c == '_' || std::isalnum(c); }
+/// Whether a char is a whitespace.
+inline bool is_whitespace(char c) { return std::isblank(c); }
+} // namespace detail
 
-enum class IdentifierType {
-  type_name,
-  template_name,
-  qualifier,
-  end
-};
+/// Type of identifiers.
+enum class IdentifierType { type_name, template_name, qualifier, end };
 
 ////////////////////////////////////////////////////////////////////////////////
 // Identifier iterator
 ////////////////////////////////////////////////////////////////////////////////
 
+/** Iterate over identifiers in type expressions.
+ *
+ * This class implements an iterator class to iterate over identifiers in
+ * C++ type expressions and to replace them if required.
+ */
 class IdentifierIterator {
-public:
-  IdentifierIterator(std::string &spelling)
-      : spelling_(spelling), position(0), token_length_(0) {
-        parse_identifier();
-  }
 
+  /// Parses next identifier in string.
   void parse_identifier();
 
-  bool operator==(const IdentifierIterator &other) {
-    return (spelling_ == other.spelling_) && (position == other.position);
-  }
-
+  /// Extract char and advance reading position.
   char consume() {
-      char c = 0;
-      if (position < spelling_.size() - 1) {
-          ++position;
-          c = spelling_[position];
-      }
-      return c;
+    char c = 0;
+    if (position_ < (spelling_->size() - 1)) {
+      ++position_;
+      c = (*spelling_)[position_];
+    }
+    return c;
   }
 
+  /// Return current char.
   char get_current_char() {
-      char c = 0;
-      if (position < spelling_.size()) {
-          c = spelling_[position];
-      }
-      return c;
+    char c = 0;
+    if (position_ < spelling_->size()) {
+      c = (*spelling_)[position_];
+    }
+    return c;
   }
 
+public:
+  /** Create IdentifierIterator to iterate over type expression.
+   *
+   * @param spelling The type expression given as string.
+   */
+  IdentifierIterator(std::string &spelling)
+      : spelling_(&spelling), position_(0), token_length_(0) {
+    parse_identifier();
+  }
 
-  IdentifierIterator &operator++() { parse_identifier(); return *this;}
+  /** Two iterators are the same if the refer to the same expression
+   * and are at the same position.
+   */
+  bool operator==(const IdentifierIterator &other) {
+    return (spelling_ == other.spelling_) && (position_ == other.position_);
+  }
 
+  /// Move to next identifier.
+  IdentifierIterator &operator++() {
+    parse_identifier();
+    return *this;
+  }
+  /// Return the type of the identifier.
   IdentifierType get_type() const { return type_; }
+  /// Return the current identifier as string.
+  std::string get_identifier() {
+    return std::string(*spelling_, token_start_, token_length_);
+  }
+  /// Check if iterator is exhausted.
+  operator bool() const { return type_ != IdentifierType::end; }
 
-  std::string get_identifier() { return std::string(spelling_, token_start_, token_length_); }
+  void operator=(std::string replacement) {
+    spelling_->replace(token_start_, token_length_, replacement);
+    position_ = token_start_ + replacement.size();
+    token_length_ = replacement.size();
+  }
 
 private:
-  const std::string &spelling_;
-  size_t position, token_start_, token_length_;
+  std::string *spelling_;
+  size_t position_, token_length_, token_start_;
   IdentifierType type_;
 };
 
 void IdentifierIterator::parse_identifier() {
 
-    token_length_ = 0;
-    type_ = IdentifierType::end;
+  token_length_ = 0;
+  type_ = IdentifierType::end;
 
-    char current_char = get_current_char();
+  char current_char = get_current_char();
+  // Skip anything not part of an identifier.
+  while (!detail::is_identifier_char(current_char) && (current_char != 0)) {
+    current_char = consume();
+  }
 
-    // Skip anything not part of an identifier.
-    while (!detail::is_identifier_char(current_char)) {
-        current_char = consume();
-    }
+  // Did we reach the end?
+  if (current_char == 0) {
+    return;
+  }
 
-    // Did we reach the end?
-    if (current_char == 0) {
-        return;
-    }
+  token_start_ = position_;
+  // Parse identifier sequence.
+  while (detail::is_identifier_char(current_char)) {
+    current_char = consume();
+    ++token_length_;
+  }
 
-    token_start_ = position;
-    // Parse identifier sequence.
-    while (detail::is_identifier_char(current_char)) {
-        current_char = consume();
-        ++token_length_;
-    }
+  // Determine type
+  if (current_char == ':') {
+    type_ = IdentifierType::qualifier;
+  } else if (current_char == '<') {
+    type_ = IdentifierType::template_name;
+  } else {
+    type_ = IdentifierType::type_name;
+  }
 
-    // Determine type
-    if (current_char == ':') {
-        type_ = IdentifierType::qualifier;
-    } else if (current_char ==  '<') {
-        type_ = IdentifierType::template_name;
-    } else {
-        type_ = IdentifierType::type_name;
-    }
+  if (is_keyword(get_identifier())) {
+    parse_identifier();
+  }
+  current_char = get_current_char();
 
-    if (is_keyword(get_identifier()))  {
-        parse_identifier();
-    }
+  // Skip ahead to beginning of next identifier.
+  while ((!detail::is_identifier_char(current_char)) && (current_char != 0)) {
+    current_char = consume();
+  }
 }
 
-//class TypeExpression {
-//public:
-//  TypeExpression(std::string spelling) : spelling_(spelling) {}
-//
-//  TypeExpressionToken get_next(size_t starting_position) {
-//
-//    TypeExpressionToken token;
-//    size_t current_position = starting_position;
-//
-//    // Skip whitespace.
-//    while (detail::is_whitespace(spelling_(current_position))) {
-//      ++current_position;
-//    }
-//
-//    // Start extracting the token text.
-//    size_t start_of_identifier = current_position;
-//    token.start = current_position;
-//    while (detail::is_identifier_char(spelling(current_position))) {
-//      current_position++;
-//    }
-//    token.length = current_position - start_of_identifier;
-//    if (current_position == spelling_.size()) {
-//      return TypeExpressionToken(spelling_,
-//                                 start_of_identifier,
-//                                 current_position - start_of_identifier,
-//          )
-//    }
-//  }
-//
-//}
-//
-//std::string spelling_;
-//};
-
-}
+} // namespace types
 } // namespace cxx
 } // namespace pxx
--- test/unit_tests/cxx/test_type_expression.cpp
+++ test/unit_tests/cxx/test_type_expression.cpp
@@ -6,22 +6,77 @@ TEST_CASE( "identifier_iterator", "[cxx/type_name]" ) {
     using pxx::cxx::types::IdentifierIterator;
     using pxx::cxx::types::IdentifierType;
 
-    std::string test_type = "const my_namespace::Template<int, MyType>";
+    std::string test_type = "const std::my_namespace::Template<int, MyType>";
 
     auto iterator = IdentifierIterator(test_type);
 
+    REQUIRE(iterator);
+    REQUIRE(iterator.get_identifier() == "std");
+    REQUIRE(iterator.get_type() == IdentifierType::qualifier);
+
+    ++iterator;
+    REQUIRE(iterator);
     REQUIRE(iterator.get_identifier() == "my_namespace");
     REQUIRE(iterator.get_type() == IdentifierType::qualifier);
 
     ++iterator;
+    REQUIRE(iterator);
     REQUIRE(iterator.get_identifier() == "Template");
     REQUIRE(iterator.get_type() == IdentifierType::template_name);
 
     ++iterator;
+    REQUIRE(iterator);
     REQUIRE(iterator.get_identifier() == "MyType");
     REQUIRE(iterator.get_type() == IdentifierType::type_name);
 
+    ++iterator;
+    REQUIRE(iterator.get_type() == IdentifierType::end);
+    REQUIRE(!iterator);
 
+    test_type = "void (*)(int, MyClass, &MyOtherClass)";
+    iterator = IdentifierIterator(test_type);
+    REQUIRE(iterator.get_identifier() == "MyClass");
+    REQUIRE(iterator.get_type() == IdentifierType::type_name);
+    ++iterator;
+    REQUIRE(iterator.get_identifier() == "MyOtherClass");
+    REQUIRE(iterator.get_type() == IdentifierType::type_name);
+
+}
+
+TEST_CASE( "identifier_iterator_replacements", "[cxx/type_name]" ) {
+
+    using pxx::cxx::types::IdentifierIterator;
+    using pxx::cxx::types::IdentifierType;
+
+    std::string test_type = "const std::my_namespace::Template<int, MyType>";
+
+    auto iterator = IdentifierIterator(test_type);
+
+    REQUIRE(iterator);
+    REQUIRE(iterator.get_identifier() == "std");
+    REQUIRE(iterator.get_type() == IdentifierType::qualifier);
+    iterator = "my_namespace";
+    REQUIRE(iterator.get_identifier() == "my_namespace");
+
+    ++iterator;
+    REQUIRE(iterator);
+    REQUIRE(iterator.get_identifier() == "my_namespace");
+    REQUIRE(iterator.get_type() == IdentifierType::qualifier);
+    iterator = "std";
+    REQUIRE(iterator.get_identifier() == "std");
+
+    ++iterator;
+    REQUIRE(iterator);
+    REQUIRE(iterator.get_identifier() == "Template");
+    REQUIRE(iterator.get_type() == IdentifierType::template_name);
+
+    ++iterator;
+    REQUIRE(iterator);
+    REQUIRE(iterator.get_identifier() == "MyType");
+    REQUIRE(iterator.get_type() == IdentifierType::type_name);
+    iterator = "MyOtherType";
+
+    REQUIRE(test_type == "const my_namespace::std::Template<int, MyOtherType>");
 
 
 }
