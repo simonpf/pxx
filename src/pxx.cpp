ac11d24bfdceb9b986fa5f4c7188bd786517a9b2
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2022-10-07 17:19:51 +0200

    Updated python bindings.

--- src/pxx.cpp
+++ src/pxx.cpp
@@ -1,15 +1,21 @@
 #define PY_SSIZE_T_CLEAN
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pxx/generator.h>
+
+#include <pxx/settings.h>
+#include <pxx/cxx/translation_unit.h>
 
 namespace py = pybind11;
 using namespace pybind11::literals;
 
 PYBIND11_MODULE(_pxx, m) {
 
-  py::class_<pxx::Generator>(m, "Generator")
-      .def(py::init<std::string, std::vector<std::string>>())
-      .def("print_bindings", &pxx::Generator::print_bindings)
-      .def("print_ast", &pxx::Generator::print_ast);
+py::class_<pxx::Settings>(m, "Settings")
+    .def(py::init<>())
+    .def_readwrite("header", &pxx::Settings::header)
+    .def_readwrite("includes", &pxx::Settings::includes);
+py::class_<pxx::cxx::TranslationUnit>(m, "TranslationUnit")
+    .def(py::init<std::string, std::vector<std::string>>())
+    .def("dump_ast", &pxx::cxx::TranslationUnit::dump_ast)
+    .def("print_bindings", &pxx::cxx::TranslationUnit::print_bindings);
 }
